2912f9c6e01118729924f378780c171334cf101d
Gleb Stachev <stachev.gleb@gmail.com>
2021-03-20 13:50:13 +0200

    update(js, css, html):inputs,functions;decoder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let stringAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowAlph = stringAlphabet.toLowerCase();
 let alphabet = lowAlph.split('');
 // console.log('Alphbet', alphabet);
-
+let output = document.getElementById('outputInner');
 
 let shiftedAlphabet;
 
@@ -15,7 +15,7 @@ function valueIn(){
     console.log(valueGet1);
 }
 
-btn1.addEventListener('click', valueIn);
+// btn1.addEventListener('click', valueIn);
 
 let btn2 = document.getElementById('btn2');
 
@@ -34,7 +34,7 @@ function valueNum(){
     shiftedAlphabet +=(","+alphabet.slice(0, valueGet2));
     // console.log(shiftedAlphabet);
     let lowShiftedAlph = shiftedAlphabet.toLowerCase();
-  // console.log(lowShiftedAlph);
+    // console.log(lowShiftedAlph);
     let cipheredAlphabet;
 
     cipheredAlphabet = lowShiftedAlph.split(",");
@@ -45,7 +45,51 @@ function valueNum(){
         console.log(number);
         let result = cipheredAlphabet[number];
         console.log(result);
+        output.innerHTML += result;
     }
 }
 
 btn2.addEventListener('click', valueNum);
+
+
+let outputDecoder = document.getElementById('outputInnerDecoder');
+let btn2Decoder = document.getElementById('btn2Decoder');
+
+
+function valueNum1(){
+    let msg = document.getElementById('inputTextDecoder');
+    let stringMsg = msg.value;
+    let lowMsg = stringMsg.toLowerCase();
+    let msgArray = lowMsg.split('');
+
+    let input = document.getElementById('inputNumber');
+    let valueGet2 = input.value;
+    let valueGetOutput;
+    
+    if (valueGet2 > 0 ) {
+        valueGetOutput = -valueGet2;
+        console.log('valueGet2 > 0', valueGetOutput)
+    }
+     else if (valueGet2 < 0 ){
+         valueGetOutput = (-1) * valueGet2;
+         console.log('valueGet2 < 0', valueGetOutput)
+        }
+        
+    shiftedAlphabet = alphabet.slice(valueGetOutput);
+    shiftedAlphabet +=(","+alphabet.slice(0, valueGetOutput));
+    let lowShiftedAlph = shiftedAlphabet.toLowerCase();
+    let cipheredAlphabet;
+
+    cipheredAlphabet = lowShiftedAlph.split(",");
+    console.log(cipheredAlphabet);
+
+    for (let i = 0; i < msgArray.length; i++){
+        let number = alphabet.indexOf(msgArray[i]);
+        console.log(number);
+        let result = cipheredAlphabet[number];
+        console.log(result);
+        outputDecoder.innerHTML += result;
+    }
+}
+
+btn2Decoder.addEventListener('click', valueNum1);
